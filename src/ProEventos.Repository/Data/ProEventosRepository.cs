31025468c4eec75c628226f2cf5e3f2db6f03f76
Ronaldo Vasconcelos <ronaldo.asconcellos@gmail.com>
2021-03-23 16:35:31 -0300

    create context

---
+++ src/ProEventos.Repository/Data/ProEventosRepository.cs
@@ -0,0 +1,32 @@
+using Microsoft.EntityFrameworkCore;
+using ProEventos.Domain;
+
+namespace src.ProEventos.Repository.Data
+{
+    public class ProEventosRepository : DbContext
+    {
+        public ProEventosRepository(DbContextOptions<ProEventosRepository> options) : base(options) { }
+
+        public DbSet<Event> Events { get; set; }
+        public DbSet<Lot> Lots { get; set; }
+        public DbSet<Speaker> Speakers { get; set; }
+        public DbSet<EventSpeaker> EventSpeaker { get; set; }
+        public DbSet<SocialNetWorker> SocialMedia { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<EventSpeaker>()
+                .HasKey(es => new { es.EventId, es.SpeakerId });
+
+            modelBuilder.Entity<Event>()
+                .HasMany(e => e.SocialMedia)
+                .WithOne(es => es.Event)
+                .OnDelete(DeleteBehavior.Cascade);
+
+            modelBuilder.Entity<Speaker>()
+                .HasMany(es => es.SocialMedia)
+                .WithOne(es => es.Speaker)
+                .OnDelete(DeleteBehavior.Cascade);
+        }
+    }
+}
