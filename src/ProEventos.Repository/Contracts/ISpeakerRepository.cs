11d1df7724b1d58d2d50a8816504b69cc3e4e4cf
Ronaldo Vasconcelos <ronaldo.asconcellos@gmail.com>
2021-03-23 20:35:11 -0300

    commit aprcial

--- src/ProEventos.Repository/Contracts/ISpeakerRepository.cs
+++ src/ProEventos.Repository/Contracts/ISpeakerRepository.cs
@@ -2,7 +2,7 @@ using System;
 using System.Threading.Tasks;
 using ProEventos.Domain;
 
-namespace src.ProEventos.Repository.Contracts
+namespace ProEventos.Repository.Contracts
 {
     public interface ISpeakerRepository
     {
