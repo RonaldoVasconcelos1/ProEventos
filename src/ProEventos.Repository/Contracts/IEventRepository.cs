0881f960423b87cb2f1dd6199f8e9befe21fb77c
Ronaldo Vasconcelos <ronaldo.asconcellos@gmail.com>
2021-03-23 22:19:03 -0300

    fix

--- src/ProEventos.Repository/Contracts/IEventRepository.cs
+++ src/ProEventos.Repository/Contracts/IEventRepository.cs
@@ -7,7 +7,7 @@ namespace ProEventos.Repository.Contracts
     public interface IEventRepository
     {
         Task<Event[]> GetAllEventsAsync(bool includeSpeaker);
-        Task<Event[]> GetAllEventById(Guid id, bool includeSpeaker);
+        Task<Event> GetAllEventById(Guid id, bool includeSpeaker);
         Task<Event[]> GetAllEventsByTheme(string theme, bool includeSpeaker);
 
     }
