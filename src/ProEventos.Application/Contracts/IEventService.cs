10db2273d5ee6e8ffbfeb716a8ec5dea6b781d70
Ronaldo Vasconcelos <ronaldo.asconcellos@gmail.com>
2021-03-24 00:46:57 -0300

    created folder Contracts and Interface EventService

---
+++ src/ProEventos.Application/Contracts/IEventService.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Threading.Tasks;
+using ProEventos.Domain;
+
+namespace src.ProEventos.Application.Contracts
+{
+    public interface IEventService
+    {
+        Task<Event> Add(Event model);
+        Task<Event> Update(Event model, Guid id);
+        Task<bool> Remove(Guid id);
+
+        Task<Event[]> GetAllEventsAsync(bool includeSpeaker);
+        Task<Event[]> GetAllEventThemeAsync(string theme, bool includeSpeaker);
+        Task<Event> GetEventById(Guid id, bool includeSpeaker);
+    }
+}
